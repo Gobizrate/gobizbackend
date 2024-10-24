f7135290e049642b39ae38a04011d068ea65d0ab
barganakukuhraditya <barganakukuh@gmail.com>
2024-10-24 15:43:46 +0700

    Add DeleteRatingByID function to handle token validation

--- controller/rating.go
+++ controller/rating.go
@@ -38,3 +38,18 @@ func PostRating(respw http.ResponseWriter, req *http.Request) {
 
 	at.WriteJSON(respw, http.StatusOK, payload)
 }
+
+func DeleteRatingByID(respw http.ResponseWriter, req *http.Request) {
+	payload, err := watoken.Decode(config.PublicKeyWhatsAuth, at.GetLoginFromHeader(req))
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error: Token Tidak Valid"
+		respn.Info = at.GetSecretFromHeader(req)
+		respn.Location = "Decode Token Error"
+		respn.Response = err.Error()
+		at.WriteJSON(respw, http.StatusForbidden, respn)
+		return
+	}
+
+	at.WriteJSON(respw, http.StatusOK, payload)
+}
