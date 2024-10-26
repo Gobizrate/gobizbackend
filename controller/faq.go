524534d2c8b1ae4ac5d63b3ecea49416f2295200
barganakukuhraditya <barganakukuh@gmail.com>
2024-10-26 12:50:37 +0700

    Add GetAllFAQ function

---
+++ controller/faq.go
@@ -0,0 +1,25 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gocroot/config"
+	"github.com/gocroot/helper/at"
+	"github.com/gocroot/helper/watoken"
+	"github.com/gocroot/model"
+)
+
+func GetAllFAQ(respw http.ResponseWriter, req *http.Request) {
+	payload, err := watoken.Decode(config.PublicKeyWhatsAuth, at.GetLoginFromHeader(req))
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error: Token Tidak Valid"
+		respn.Info = at.GetSecretFromHeader(req)
+		respn.Location = "Decode Token Error"
+		respn.Response = err.Error()
+		at.WriteJSON(respw, http.StatusForbidden, respn)
+		return
+	}
+
+	at.WriteJSON(respw, http.StatusOK, payload)
+}
