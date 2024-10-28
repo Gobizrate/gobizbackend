4b3def7842002fb71da213310748c78675f2123e
barganakukuhraditya <barganakukuh@gmail.com>
2024-10-28 08:33:38 +0700

    Add PostFAQ function to handle token validation

--- controller/faq.go
+++ controller/faq.go
@@ -23,3 +23,18 @@ func GetAllFAQ(respw http.ResponseWriter, req *http.Request) {
 
 	at.WriteJSON(respw, http.StatusOK, payload)
 }
+
+func PostFAQ(respw http.ResponseWriter, req *http.Request) {
+	payload, err := watoken.Decode(config.PublicKeyWhatsAuth, at.GetLoginFromHeader(req))
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error: Token Tidak Valid"
+		respn.Info = at.GetSecretFromHeader(req)
+		respn.Location = "Decode Token Error"
+		respn.Response = err.Error()
+		at.WriteJSON(respw, http.StatusForbidden, respn)
+		return
+	}
+
+	at.WriteJSON(respw, http.StatusOK, payload)
+}
