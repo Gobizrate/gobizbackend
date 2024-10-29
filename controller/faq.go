8f47330a5269eb1023ab690f6e5b753108a08c6f
barganakukuhraditya <barganakukuh@gmail.com>
2024-10-29 16:33:31 +0700

    Add UpdateFAQ function for updating FAQs

--- controller/faq.go
+++ controller/faq.go
@@ -53,3 +53,18 @@ func DeleteFAQByID(respw http.ResponseWriter, req *http.Request) {
 
 	at.WriteJSON(respw, http.StatusOK, payload)
 }
+
+func UpdateFAQ(respw http.ResponseWriter, req *http.Request) {
+	payload, err := watoken.Decode(config.PublicKeyWhatsAuth, at.GetLoginFromHeader(req))
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error: Token Tidak Valid"
+		respn.Info = at.GetSecretFromHeader(req)
+		respn.Location = "Decode Token Error"
+		respn.Response = err.Error()
+		at.WriteJSON(respw, http.StatusForbidden, respn)
+		return
+	}
+
+	at.WriteJSON(respw, http.StatusOK, payload)
+}
